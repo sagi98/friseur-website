1a2c03c2db62d784884a8681ecf5fb90ffb702b9
Sagi <266722112+sagi98@users.noreply.github.com>
2026-03-09 15:33:31 +0100

    update homepage layout

--- app/page.js
+++ app/page.js
@@ -1,13 +1,25 @@
+"use client";
+
+import { useState } from "react";
 import "./stylesheet.css";
 
 export default function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <main className="page">
       <header className="header">
         <div className="header-top">
-          <div className="logo">Arshad & Rewano</div>
+          <div className="logo">
+            <img src="/logo.png" alt="Arshad & Rewano Logo" />
+          </div>
 
-          <button className="menu-button" aria-label="Menü öffnen">
+          <button
+            className={`menu-button ${menuOpen ? "open" : ""}`}
+            aria-label="Menü öffnen"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span></span>
             <span></span>
             <span></span>
           </button>
